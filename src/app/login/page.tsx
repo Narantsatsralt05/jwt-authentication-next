2e98780d63efbea5303f04fef75c18370e448010
Narantsatsralt05 <n.bumnasan@gmail.com>
2025-02-12 16:24:48 +0800

    first commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,121 @@
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import { useToast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+const formSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6).max(255),
+});
+
+export default function LoginForm() {
+  const { toast } = useToast();
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async ({ email, password }: z.infer<typeof formSchema>) => {
+    setLoading(true);
+    const res = await axios.post(
+      "http://localhost:3000" + "/auth/sign-in",
+      {
+        email,
+        password,
+      },
+      { withCredentials: true }
+    );
+    setLoading(false);
+
+    if (!res.data.success) {
+      return toast({
+        title: "Login",
+        description: res.data.message,
+      });
+    }
+
+    toast({
+      title: "Login",
+      description: res.data.message,
+    });
+    router.push("/");
+  };
+
+  return (
+    <div className="flex items-center h-screen">
+      <Card className="w-96 m-auto">
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+          {/* <CardDescription>Card Description</CardDescription> */}
+        </CardHeader>
+        <CardContent className="flex flex-col gap-8">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button className="min-w-32" disabled={loading} type="submit">
+                {loading && <span className="loader w-2 h-2"></span>}
+                Submit
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
+        <CardFooter>
+          <p className="text-sm text-muted-foreground">@copyright Pinecone</p>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
