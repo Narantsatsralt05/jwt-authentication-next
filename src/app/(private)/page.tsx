2e89cd015127d86c0eba2e783da2498160c5ae46
Narantsatsralt05 <n.bumnasan@gmail.com>
2025-02-18 16:36:42 +0800

    add middleware

--- src/app/(private)/page.tsx
+++ src/app/(private)/page.tsx
@@ -1,14 +1,18 @@
 "use client";
 
 import { useEffect } from "react";
+import Cookies from 'js-cookie'
 export default function Home() {
   const getUsers = async () => {
     const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL ?? process.env.NEXT_PUBLIC_LOCAL_BACKEND_URL
+    const accessToken =  Cookies.get('accessToken')
 
     const res = await fetch(
       BASE_URL + "/users",
       {
-        credentials: "include",
+        headers: {
+          Authorization: 'Bearer ' + accessToken
+        },
       }
     );
     const result = await res.json();
